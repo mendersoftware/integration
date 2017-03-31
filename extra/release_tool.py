b8b524776e2dbe7a5bd211a78516fe33cd274206
Kristian Amlie <kristian.amlie@mender.io>
2017-03-31 16:39:24 +0200

    release_tool: Make sure tags are included when creating temp checkout.
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

--- extra/release_tool.py
+++ extra/release_tool.py
@@ -269,7 +269,7 @@ def setup_temp_git_checkout(state, repo_git, branch):
 
     execute_git(state, repo_git, ["init", "tmp_checkout"], capture=True)
     execute_git(state, tmpdir, ["fetch", os.path.join(state['repo_dir'], repo_git),
-                                "refs/remotes/%s:%s" % (branch, branch)], capture=True)
+                                "--tags", "%s:%s" % (branch, branch)], capture=True)
     execute_git(state, tmpdir, ["checkout", "-t", branch], capture=True)
 
     return tmpdir

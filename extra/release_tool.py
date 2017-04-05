5bbf385a6d07a3ee3fc4adb2176cae12e7a53108
Kristian Amlie <kristian.amlie@mender.io>
2017-04-05 13:50:47 +0200

    release_tool: Add lots of code comments.
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>
    (cherry picked from commit 34675391b6dac34c1dfeead8d37e4ca10eacbeda)

--- extra/release_tool.py
+++ extra/release_tool.py
@@ -27,11 +27,11 @@ os.environ['GIT_PAGER'] = "cat"
 # The repositories are indexed by their Git repository names.
 RELEASE_STATE = "release-state.yml"
 
-#JENKINS_SERVER = "http://localhost:8080"
 JENKINS_SERVER = "https://ci.cfengine.com"
 JENKINS_JOB = "job/yoctobuild-kristian"
 JENKINS_CRUMB_ISSUER = 'crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
 
+# What we use in commits messages when bumping versions.
 VERSION_BUMP_STRING = "Bump versions for Mender"
 
 # Whether or not pushes should really happen.
@@ -43,7 +43,7 @@ class RepoName:
     """An object that contains a pair of links for the docker and git names of a
     repository."""
 
-    # Name of container is docker-compose file.
+    # Name of container in docker-compose file.
     container = None
     # Name of image in docker hub
     docker = None
@@ -58,8 +58,14 @@ class RepoName:
         self.git = git
         self.has_container = has_container
 
-# All our repos, and also a map from docker-compose container name to all
-# names. Everywhere we index using the Git name, unless specified otherwise.
+# All our repos, and also a map from docker-compose image name to all
+# names. The key is container name, and thereafter the order is the order of the
+# RepoName constructor, just above.
+#
+# This is the main list of repos that will be used throughout the script. If you
+# add anything here, make sure to also update REPO_ALIASES (if there are
+# alternate names) and GIT_TO_BUILDPARAM_MAP (which tells the tool how to
+# trigger Jenkins jobs.
 REPOS = {
     "mender-api-gateway": RepoName("mender-api-gateway", "api-gateway", "mender-api-gateway-docker", True),
     "mender-client": RepoName("mender-client", "mender-client-qemu", "mender", True),
@@ -87,6 +93,7 @@ REPO_ALIASES = {
     "mender-api-gateway-docker": "mender-api-gateway",
 }
 
+# A map from git repo name to build parameter name in Jenkins.
 GIT_TO_BUILDPARAM_MAP = {
     "mender-api-gateway-docker": "API_GATEWAY_REV",
     "deployments": "DEPLOYMENTS_REV",
@@ -118,9 +125,13 @@ EXTRA_BUILDPARAMS = {
 }
 
 def integration_dir():
+    """Return the location of the integration repository."""
+
     return os.path.join(os.path.dirname(sys.argv[0]), "..")
 
 def ask(text):
+    """Ask a question and return the reply."""
+
     sys.stdout.write(text)
     sys.stdout.flush()
     reply = sys.stdin.readline().strip()
@@ -140,15 +151,22 @@ def deepupdate(original, update):
             original[key] = value
 
 def determine_repo(repoish):
+    """Based on a repository name, which can be any variant of Docker or Git
+    name, return the Repo object assosiated with it."""
+
+    # Try aliases first.
     alias = REPO_ALIASES.get(repoish)
     if alias is not None:
         repoish = alias
+
+    # Prepend mender if it doesn't start with, since our index map uses that.
     if not repoish.startswith("mender-"):
         repoish = "mender-" + repoish
+
     return REPOS[repoish]
 
 def docker_compose_files_list(dir):
-    """Return all docker-compose*.yml files in this directory."""
+    """Return all docker-compose*.yml files in given directory."""
     list = []
     for entry in os.listdir(dir):
         if entry.startswith("docker-compose") and entry.endswith(".yml"):
@@ -156,7 +174,7 @@ def docker_compose_files_list(dir):
     return list
 
 def get_docker_compose_data(dir):
-    """Return a structure of data from all the docker-compose YAML files."""
+    """Return the Yaml as data from all the docker-compose YAML files."""
     data = {}
     for file in docker_compose_files_list(dir):
         fd = open(file)
@@ -165,6 +183,8 @@ def get_docker_compose_data(dir):
     return data
 
 def do_version_of(args):
+    """Process --version-of argument."""
+
     data = get_docker_compose_data(integration_dir())
     try:
         repo = determine_repo(args.version_of)
@@ -177,7 +197,13 @@ def do_version_of(args):
 def state_value(state, key_list):
     """Gets a value from the state variable stored in the RELEASE_STATE yaml
     file. The key_list is a list of indexes, where each element represents a
-    subkey of the previous key."""
+    subkey of the previous key.
+
+    The difference between this function and simply indexing 'state' directly is
+    that if any subkey is not found, including parent keys, None is returned
+    instead of exception.
+    """
+
     try:
         next = state
         for key in key_list:
@@ -204,7 +230,11 @@ def update_state(state, key_list, value):
 
 def execute_git(state, repo_git, args, capture=False, capture_stderr=False):
     """Executes a Git command in the given repository, with args being a list
-    of arguments (not including git itself)."""
+    of arguments (not including git itself). capture and capture_stderr
+    arguments causes it to return stdout or stdout+stderr as a string.
+
+    The function automatically takes into account Git commands with side effects
+    and applies push simulation and dry run if those are enabled."""
 
     is_push = (args[0] == "push")
     is_change = (is_push
@@ -265,7 +295,11 @@ def query_execute_git_list(execute_git_list):
 
 def query_execute_list(execute_list):
     """Executes the list of commands after asking first. The argument is a list of
-    lists, where the inner list is the argument to subprocess.check_call."""
+    lists, where the inner list is the argument to subprocess.check_call.
+
+    The function automatically takes into account Docker commands with side
+    effects and applies push simulation and dry run if those are enabled.
+    """
 
     print("--------------------------------------------------------------------------------")
     for cmd in execute_list:
@@ -289,7 +323,7 @@ def query_execute_list(execute_list):
 
 def setup_temp_git_checkout(state, repo_git, branch):
     """Checks out a temporary Git directory, and returns an absolute path to
-    it. Checks out the branch specified in repo_git."""
+    it. Checks out the branch specified in branch."""
 
     tmpdir = os.path.join(state['repo_dir'], repo_git, "tmp_checkout")
     cleanup_temp_git_checkout(tmpdir)
@@ -312,6 +346,9 @@ def cleanup_temp_git_checkout(tmpdir):
     shutil.rmtree(tmpdir, ignore_errors=True)
 
 def find_upstream_remote(state, repo_git):
+    """Given a Git repository name, figure out which remote name is the
+    "mendersoftware" upstream."""
+
     config = execute_git(state, repo_git, ["config", "-l"], capture=True)
     remote = None
     for line in config.split('\n'):
@@ -326,6 +363,8 @@ def find_upstream_remote(state, repo_git):
     return remote
 
 def refresh_repos(state):
+    """Do a full 'git fetch' on all repositories."""
+
     git_list = []
 
     for repo in REPOS.values():
@@ -336,15 +375,30 @@ def refresh_repos(state):
     query_execute_git_list(git_list)
 
 def check_tag_availability(state):
+    """Check which tags are available in all the Git repositories, and return
+    this as the tag_avail data structure.
+
+    The main fields in this one are:
+      <repo>:
+        already_released: <whether this is a final release tag or not (true/false)>
+        build_tag: <highest build tag, or final tag>
+        following: <branch we pick next build tag from>
+        sha: <SHA of current build tag>
+    """
+
     tag_avail = {}
     for repo in REPOS.values():
         tag_avail[repo.git] = {}
         try:
             execute_git(state, repo.git, ["rev-parse", state[repo.git]['version']],
                         capture=True, capture_stderr=True)
+            # No exception happened during above call: This is a final release
+            # tag.
             tag_avail[repo.git]['already_released'] = True
             tag_avail[repo.git]['build_tag'] = state[repo.git]['version']
         except subprocess.CalledProcessError:
+            # Exception happened during Git call. This tag doesn't exist, and
+            # we must look for and/or create build tags.
             tag_avail[repo.git]['already_released'] = False
 
             # Find highest <version>-buildX tag, where X is a number.
@@ -356,7 +410,9 @@ def check_tag_availability(state):
                     highest = int(match.group(1))
                     highest_tag = tag
             if highest >= 0:
+                # Assign highest tag so far.
                 tag_avail[repo.git]['build_tag'] = highest_tag
+            # Else: Nothing. This repository doesn't have any build tags yet.
 
         if tag_avail[repo.git].get('build_tag') is not None:
             sha = execute_git(state, repo.git, ["rev-parse", "--short",
@@ -367,9 +423,13 @@ def check_tag_availability(state):
     return tag_avail
 
 def repo_sort_key(repo):
+    """Used in sorted() calls to sort by Git name."""
     return repo.git
 
 def report_release_state(state, tag_avail):
+    """Reports the current state of the release, including current build
+    tags."""
+
     print("Mender release: %s" % state['version'])
     fmt_str = "%-25s %-10s %-18s %-20s"
     print(fmt_str % ("REPOSITORY", "VERSION", "PICK NEXT BUILD", "BUILD TAG"))
@@ -393,6 +453,8 @@ def report_release_state(state, tag_avail):
                          following, tag))
 
 def annotation_version(repo, tag_avail):
+    """Generates the string used in Git tag annotations."""
+
     match = re.match("^(.*)-build([0-9]+)$", tag_avail[repo.git]['build_tag'])
     if match is None:
         return "%s version %s." % (repo.git, tag_avail[repo.git]['build_tag'])
@@ -400,8 +462,9 @@ def annotation_version(repo, tag_avail):
         return "%s version %s Build %s." % (repo.git, match.group(1), match.group(2))
 
 def generate_new_tags(state, tag_avail, final):
-    """Creates new build tags, and returns the new tags in a modified
-    tag_avail."""
+    """Creates new build tags, and returns the new tags in a modified tag_avail. If
+    interrupted anywhere, it makes no change, and returns the original tag_avail
+    instead."""
 
     output = execute_git(state, "integration", ["show", "-s"], capture=True)
     if output.find(VERSION_BUMP_STRING) >= 0:
@@ -431,6 +494,8 @@ def generate_new_tags(state, tag_avail, final):
                 # For final tag, use actual version.
                 next_tag_avail[repo.git]['build_tag'] = state[repo.git]['version']
             else:
+                # For build tag, point the next tag to the last version of the
+                # branch we follow.
                 # "~0" is used to avoid a tag pointing to another tag. It should
                 # point to the commit.
                 sha = execute_git(state, repo.git, ["rev-parse", "--short",
@@ -492,6 +557,7 @@ def generate_new_tags(state, tag_avail, final):
     finally:
         cleanup_temp_git_checkout(tmpdir)
 
+    # Prepare Git tag and push commands.
     git_list = []
     for repo in REPOS.values():
         if not next_tag_avail[repo.git]['already_released']:
@@ -504,6 +570,7 @@ def generate_new_tags(state, tag_avail, final):
     if not query_execute_git_list(git_list):
         return tag_avail
 
+    # If this was the final tag, reflect that in our data.
     for repo in REPOS.values():
         if not next_tag_avail[repo.git]['already_released'] and final:
             next_tag_avail[repo.git]['already_released'] = True
@@ -517,8 +584,11 @@ def trigger_jenkins_build(state, tag_avail):
         print("PyYAML missing, try running 'sudo pip3 install requests'.")
         sys.exit(2)
 
+    # We'll be adding parameters here that shouldn't be in 'state', so make a
+    # copy.
     params = copy.deepcopy(state['extra_buildparams'])
 
+    # Populate parameters with build tags for each repository.
     postdata = []
     for repo in sorted(REPOS.values(), key=repo_sort_key):
         if tag_avail[repo.git].get('build_tag') is None:
@@ -526,6 +596,7 @@ def trigger_jenkins_build(state, tag_avail):
             return
         params[GIT_TO_BUILDPARAM_MAP[repo.git]] = tag_avail[repo.git]['build_tag']
 
+    # Allow changing of build parameters.
     while True:
         print("--------------------------------------------------------------------------------")
         fmt_str = "%-25s %-20s"
@@ -548,7 +619,9 @@ def trigger_jenkins_build(state, tag_avail):
         params[name] = ask("Ok. New value? ")
 
         if EXTRA_BUILDPARAMS.get(name) is not None:
-            # Save extra build parameters persistently.
+            # Extra build parameters, that are not part of the build tags for
+            # each repository, should be saved persistently in the state file so
+            # that they can be repeated in subsequent builds.
             update_state(state, ['extra_buildparams', name], params[name])
 
     # Order is important here, because Jenkins passes in the same parameters
@@ -602,6 +675,9 @@ def trigger_jenkins_build(state, tag_avail):
         traceback.print_exc()
 
 def set_docker_compose_version_to(dir, repo_docker, tag):
+    """Modifies docker-compose files in the given directory so that repo_docker
+    image points to the given tag."""
+
     compose_files = docker_compose_files_list(dir)
     for file in compose_files:
         old = open(file)
@@ -616,6 +692,10 @@ def set_docker_compose_version_to(dir, repo_docker, tag):
         os.rename(file + ".tmp", file)
 
 def purge_build_tags(state, tag_avail):
+    """Gets rid of all tags in all repositories that match the current version
+    of each repository and ends in '-build[0-9]+'. Then deletes this from
+    upstream as well."""
+
     git_list = []
     for repo in REPOS.values():
         remote = find_upstream_remote(state, repo.git)
@@ -631,6 +711,9 @@ def purge_build_tags(state, tag_avail):
     query_execute_git_list(git_list)
 
 def switch_following_branch(state, tag_avail):
+    """Switches all followed branches in all repositories that aren't released,
+    between local and remote branch."""
+
     current = None
     for repo in REPOS.values():
         if not tag_avail[repo.git]['already_released']:
@@ -651,19 +734,27 @@ def assign_default_following_branch(state, repo):
     update_state(state, [repo.git, 'following'], "%s/%s" % (remote, branch))
 
 def merge_release_tag(state, tag_avail, repo):
+    """Merge tag into version branch, but only for Git history's sake, the 'ours'
+    merge strategy keeps the branch as it is, the changes in the tag are not
+    pulled in. Without this merge, Git won't auto-grab tags without using "git
+    fetch --tags", which is inconvenient for users.
+    """
+
     if not tag_avail[repo.git]['already_released']:
         print("Repository must have a final release tag before the tag can be merged!")
         return
 
+    # Do the job in a temporary Git repo. Note that we check out the currently
+    # followed branch, which may theoretically be later than the released tag.
+    # This is because this needs to be pushed to the tip of the branch, not to
+    # where the tag is.
     tmpdir = setup_temp_git_checkout(state, repo.git, state[repo.git]['following'])
     try:
+        # Get a branch name for the currently checked out branch.
         branch = execute_git(state, tmpdir, ["symbolic-ref", "--short", "HEAD"],
                              capture=True)
 
-        # Merge tag into version branch, but only for Git history's sake, the
-        # 'ours' merge strategy keeps the branch as it is, the changes in the
-        # tag are not pulled in. Without this merge, Git won't auto-grab tags
-        # without using "git fetch --tags", which is inconvenient for users.
+        # Merge the tag into it.
         git_list = [((state, tmpdir, ["merge", "-s", "ours", "-m",
                                       "Merge tag %s into %s using 'ours' merge strategy."
                                       % (tag_avail[repo.git]['build_tag'], branch),
@@ -671,8 +762,11 @@ def merge_release_tag(state, tag_avail, repo):
         if not query_execute_git_list(git_list):
             return
 
+        # And then fetch that object back into the original repository, which
+        # remains untouched.
         execute_git(state, repo.git, ["fetch", tmpdir, branch])
 
+        # Push it to upstream.
         upstream = find_upstream_remote(state, repo.git)
         git_list = [((state, repo.git, ["push", upstream, "FETCH_HEAD:refs/heads/%s"
                                         % branch]))]
@@ -682,6 +776,8 @@ def merge_release_tag(state, tag_avail, repo):
         cleanup_temp_git_checkout(tmpdir)
 
 def push_latest_docker_tags(state, tag_avail):
+    """Make all the Docker ":latest" tags point to the current release."""
+
     for repo in REPOS.values():
         if not tag_avail[repo.git]['already_released']:
             print('You cannot push the ":latest" Docker tags without making final release tags first!')
@@ -723,6 +819,8 @@ def push_latest_docker_tags(state, tag_avail):
         query_execute_list(exec_list)
 
 def do_release():
+    """Handles the interactive menu for doing a release."""
+
     if os.path.exists(RELEASE_STATE):
         while True:
             reply = ask("Release already in progress. Continue or start a new one [C/S]? ")
@@ -738,6 +836,7 @@ def do_release():
         print("No existing release in progress, starting new one...")
         new_release = True
 
+    # Fill the state data.
     if new_release:
         state = {}
     else:
@@ -766,6 +865,7 @@ def do_release():
     if input.startswith("Y") or input.startswith("y"):
         refresh_repos(state)
 
+    # Fill data about available tags.
     tag_avail = check_tag_availability(state)
 
     for repo in REPOS.values():
@@ -846,6 +946,8 @@ def do_release():
             print("Invalid choice!")
 
 def do_set_version_to(args):
+    """Handles --set-version-of argument."""
+
     if args.version is None:
         print("--set-version-of requires --version")
         sys.exit(1)

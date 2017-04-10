c7fd79463fc8c4ba2380e72df97308291c55a3e1
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-04-10 10:09:29 +0200

    conductor: fail if a workflow or task cannot be added to conductor
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>
    
    Changelog: None

--- conductor/server/entrypoint/start_conductor.sh
+++ conductor/server/entrypoint/start_conductor.sh
@@ -10,12 +10,18 @@ shopt -s nullglob
 
 # load task definitions
 for task in /srv/tasks/*.json; do
-    curl -v -X POST -H "Content-Type: application/json" http://localhost:8080/api/metadata/taskdefs -d @$task
+    echo "-- loading task $task"
+    curl -f -v -X POST -H "Content-Type: application/json" \
+         http://localhost:8080/api/metadata/taskdefs \
+         -d @$task
 done
 
 # load workflow definitions
 for workflow in /srv/workflows/*.json; do
-    curl -v -X POST -H "Content-Type: application/json" http://localhost:8080/api/metadata/workflow -d @$workflow
+    echo "-- loading workflow $workflow"
+    curl -f -v -X POST -H "Content-Type: application/json" \
+         http://localhost:8080/api/metadata/workflow \
+         -d @$workflow
 done
 
 wait

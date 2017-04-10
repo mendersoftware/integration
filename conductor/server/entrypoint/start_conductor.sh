912e26927f66a282c05f2d1a05f68e09bcbc829e
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-04-10 10:36:59 +0200

    conductor: allow overriding conductor's address via env
    Set CONDUCTOR env variable to address of conductor's API (eg.
    http://my-conductor:9999) to override the default.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>
    
    Changelog: None

--- conductor/server/entrypoint/start_conductor.sh
+++ conductor/server/entrypoint/start_conductor.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
+CONDUCTOR=${CONDUCTOR="http://localhost:8080"}
+
+echo "-- conductor API is at ${CONDUCTOR}"
+
 # start conductor
 /app/startup.sh &
 
 # wait for conductor
 tries=0
 while [ "$tries" -lt 20 ]; do
-    if ! curl -f http://localhost:8080/api/metadata/taskdefs > /dev/null 2>&1 ; then
+    if ! curl -f "${CONDUCTOR}/api/metadata/taskdefs" > /dev/null 2>&1 ; then
         tries=$((tries + 1))
         echo "-- $(date) waiting for conductor, attempt ${tries}"
         sleep 5
@@ -27,7 +31,7 @@ shopt -s nullglob
 for task in /srv/tasks/*.json; do
     echo "-- loading task $task"
     curl -f -v -X POST -H "Content-Type: application/json" \
-         http://localhost:8080/api/metadata/taskdefs \
+         "${CONDUCTOR}/api/metadata/taskdefs" \
          -d @$task
 done
 
@@ -35,7 +39,7 @@ done
 for workflow in /srv/workflows/*.json; do
     echo "-- loading workflow $workflow"
     curl -f -v -X POST -H "Content-Type: application/json" \
-         http://localhost:8080/api/metadata/workflow \
+         "${CONDUCTOR}/api/metadata/workflow" \
          -d @$workflow
 done
 

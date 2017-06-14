6807c4da2866aedc0b09ced3d6660f9ac6a4b5ca
GregorioDiStefano <greg.distefano+github@gmail.com>
2017-06-14 17:45:57 +0200

    update backend testing  (#259)
    * add check on docker-compose return code
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * remove default argument to common_update.update_image_successful
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * add Deployments.get_deployment_overview function
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * add script to automate production environment
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * add production environment files used for testing
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * add upgrade testing environment and upgrade tests
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>
    
    * get the correct version of mender-artifacts
    
    Changelog: none
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>

---
+++ tests/production_test_env.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.7
+
+import os
+import sys
+import subprocess
+import argparse
+import conftest
+import common
+from MenderAPI import auth, adm
+sys.path.insert(0, "./tests")
+from common_update import common_update_procedure
+
+parser = argparse.ArgumentParser(description='Helper script to bring up production env and provision for upgrade testing')
+parser.add_argument('--start', dest='start', action='store_true',
+                    help='start production environment')
+
+parser.add_argument('--test-deployment', dest='deploy', action='store_true',
+                    help='start testing upgrade test procedure (used for upgrade testing)')
+
+conftest.docker_compose_instance = "testprod"
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+
+
+if args.start:
+    # add keys for production environment
+    if not os.path.exists("../keys-generated"):
+        ret = subprocess.call(["./keygen"], env={"CERT_API_CN": "localhost",
+                                                 "CERT_STORAGE_CN": "localhost"},
+                              cwd="../")
+        assert ret == 0, "failed to generate keys"
+
+    # copy production environment yml file
+    if not os.path.exists("../production-testing-env.yml"):
+        ret = subprocess.call(["cp", "extra/production-testing-env.yml", "."],
+                              cwd="../")
+        assert ret == 0, "failed to copy extra/production-testing-env.yml"
+
+    # start docker-compose
+    ret = subprocess.call(["docker-compose",
+                           "-p", "testprod",
+                           "-f", "docker-compose.yml",
+                           "-f", "docker-compose.storage.minio.yml",
+                           "-f", "production-testing-env.yml",
+                           "up", "-d"],
+                          cwd="../")
+
+    assert ret == 0, "failed to start docker-compose"
+
+if args.deploy:
+    # create account for management api
+    auth.get_auth_token()
+
+    # wait for 10 devices to be available
+    devices = adm.get_devices(10)
+    assert len(devices) == 10
+
+    # accept all devices
+    for d in devices:
+        adm.set_device_status(d["id"], "accepted")
+
+    # make sure artifact tool in current workdir is being used
+    os.environ["PATH"] = os.path.dirname(os.path.realpath(__file__)) + "/downloaded-tools" + os.pathsep + os.environ["PATH"]
+
+    # perform upgrade
+    devices_to_update = list(set([device["device_id"] for device in adm.get_devices_status("accepted", expected_devices=10)]))
+    deployment_id, artifact_id = common_update_procedure("core-image-full-cmdline-vexpress-qemu.ext4", device_type="test", devices=devices_to_update)
+
+    print("deployment_id=%s" % deployment_id)
+    print("artifact_id=%s" % artifact_id)
+    print("devices=%d" % len(devices))
--- tests/tests/mendertesting.py
+++ tests/tests/mendertesting.py
@@ -5,35 +5,41 @@ class MenderTesting(object):
     fast_cond = False
     nightly_cond = False
     aws_cond = False
+    upgrade_from = False
 
     slow = None
     fast = None
     nightly = None
 
-if pytest.config.getoption("--runslow"):
-    MenderTesting.slow_cond = True
-else:
-    MenderTesting.slow_cond = False
+if getattr(pytest, "config", False):
+    if pytest.config.getoption("--runslow"):
+        MenderTesting.slow_cond = True
+    else:
+        MenderTesting.slow_cond = False
 
-if pytest.config.getoption("--runfast"):
-    MenderTesting.fast_cond = True
-else:
-    MenderTesting.fast_cond = False
+    if pytest.config.getoption("--runfast"):
+        MenderTesting.fast_cond = True
+    else:
+        MenderTesting.fast_cond = False
 
-if pytest.config.getoption("--runnightly"):
-    MenderTesting.nightly_cond = True
-else:
-    MenderTesting.nightly_cond = False
+    if pytest.config.getoption("--runnightly"):
+        MenderTesting.nightly_cond = True
+    else:
+        MenderTesting.nightly_cond = False
 
-if pytest.config.getoption("--runs3"):
-    MenderTesting.aws_cond = True
+    if pytest.config.getoption("--runs3"):
+        MenderTesting.aws_cond = True
 
-if not MenderTesting.slow_cond and not MenderTesting.fast_cond and not MenderTesting.nightly_cond and not MenderTesting.aws_cond:
-    # Default to running everything but nightly.
-    MenderTesting.slow_cond = True
-    MenderTesting.fast_cond = True
+    if pytest.config.getoption("--upgrade-from"):
+        MenderTesting.upgrade_from = pytest.config.getoption("--upgrade-from")
 
-MenderTesting.slow = pytest.mark.skipif(not MenderTesting.slow_cond, reason="need --runslow option to run")
-MenderTesting.fast = pytest.mark.skipif(not MenderTesting.fast_cond, reason="need --runfast option to run")
-MenderTesting.nightly = pytest.mark.skipif(not MenderTesting.nightly_cond, reason="need --runnightly option to run")
-MenderTesting.aws_s3 = pytest.mark.skipif(not MenderTesting.aws_cond, reason="need --runs3 option to run")
+    if not MenderTesting.slow_cond and not MenderTesting.fast_cond and not MenderTesting.nightly_cond and not MenderTesting.aws_cond:
+        # Default to running everything but nightly.
+        MenderTesting.slow_cond = True
+        MenderTesting.fast_cond = True
+
+    MenderTesting.slow = pytest.mark.skipif(not MenderTesting.slow_cond, reason="need --runslow option to run")
+    MenderTesting.fast = pytest.mark.skipif(not MenderTesting.fast_cond, reason="need --runfast option to run")
+    MenderTesting.nightly = pytest.mark.skipif(not MenderTesting.nightly_cond, reason="need --runnightly option to run")
+    MenderTesting.aws_s3 = pytest.mark.skipif(not MenderTesting.aws_cond, reason="need --runs3 option to run")
+    MenderTesting.upgrade_from = pytest.mark.skipif(not MenderTesting.upgrade_from, reason="need --upgrade-from option to run")

5ceaebb5d66857a67ef9333d2e7b527ed5069a10
Gregorio Di Stefano <greg.distefano@gmail.com>
2018-01-19 13:42:30 +0100

    tests: log http responses when testing duplicate email test
    Changelog: none
    Signed-off-by: Gregorio Di Stefano <greg.distefano@gmail.com>

--- tests/tests/test_create_organization.py
+++ tests/tests/test_create_organization.py
@@ -71,9 +71,12 @@ class TestCreateOrganization(MenderTesting):
     def test_duplicate_email(self):
         payload = {"request_id": "123456", "organization": "tenant-foo", "email":"some.user@example.com", "password": "asdfqwer1234", "g-recaptcha-response": "foobar"}
         rsp = requests.post("https://%s/api/management/v1/tenantadm/tenants" % get_mender_gateway(), data=payload, verify=False)
+        logging.debug("first request to tenant adm returned: ", rsp.text)
         assert rsp.status_code == 202
+        
         payload = {"request_id": "123457", "organization": "tenant-foo2", "email":"some.user@example.com", "password": "asdfqwer1234", "g-recaptcha-response": "foobar"}
         rsp = requests.post("https://%s/api/management/v1/tenantadm/tenants" % get_mender_gateway(), data=payload, verify=False)
+        logging.debug("second request to tenant adm returned: ", rsp.text)
         assert rsp.status_code == 409
 
 

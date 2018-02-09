b07112a3f0a7c5bc72e38090b6698226dc48c049
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2018-02-09 13:30:09 +0100

    [PATCH] tests: make sure ssh connectivity is working before checking auth key
    Changelog: none
    
    Signed-off-by: Maciej Mrowiec <mrowiec.maciej@gmail.com>

--- tests/tests/test_preauth.py
+++ tests/tests/test_preauth.py
@@ -14,6 +14,7 @@
 #    limitations under the License.
 from mendertesting import MenderTesting
 from common_setup import *
+from common_docker import ssh_is_opened
 from MenderAPI import adm, deviceauth, inv
 import pytest
 import json
@@ -71,6 +72,7 @@ class TestPreauthBase(MenderTesting):
         assert dev_accepted['key'] == preauth_key
 
         # verify device was issued a token
+        ssh_is_opened(client)
         res = execute(Client.have_authtoken, hosts=client)
         assert res[client]
 

f800a495dabc6b97489a308956cc5df56349c90b
Kristian Amlie <kristian.amlie@northern.tech>
2019-05-02 13:26:59 +0200

    release_tool: Don't send "false" string for empty Jenkins variables.
    We need to keep track of which type it is from the originally fetched
    parameters, and make sure that empty strings stay empty, and boolean
    parameters get the "False" value.
    
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit 69a8829c1fc6c90608b7597a3da5017e9e8fee70)

--- extra/release_tool.py
+++ extra/release_tool.py
@@ -353,6 +353,14 @@ GIT_TO_BUILDPARAM_MAP = {
     "mender-qa": "MENDER_QA_REV",
 }
 
+class JenkinsParam:
+    type = None
+    value = None
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
 EXTRA_BUILDPARAMS_CACHE = None
 
 def init_jenkins_creds():
@@ -1106,14 +1114,18 @@ def get_extra_buildparams_from_jenkins():
     parameters = parameters[0]
 
     def jenkinsParamToDefaultMap(param):
-        if param.get("defaultParameterValue") is None:
-            return (param["name"], "")
         if param["type"] == "BooleanParameterDefinition":
-            return (param["name"], "on" if param["defaultParameterValue"]["value"] else "")
+            type = "bool"
+            value = "on" if param["defaultParameterValue"]["value"] else ""
         elif param["type"] == "StringParameterDefinition":
-            return (param["name"], param["defaultParameterValue"]["value"])
+            type = "string"
+            if param.get("defaultParameterValue") is None:
+                value = ""
+            else:
+                value = param["defaultParameterValue"]["value"]
         else:
             raise Exception("Parameter has unknown type %s. Don't know how to handle that!" % param["type"])
+        return (param["name"], type, value)
 
     # Add all fetched parameters that are not part of our versioned repositories
     # as extra build parameters.
@@ -1126,10 +1138,10 @@ def get_extra_buildparams_from_jenkins():
                 # Break out of innermost loop.
                 break
 
-    for key, value in [jenkinsParamToDefaultMap(param) for param in parameters]:
+    for key, type, value in [jenkinsParamToDefaultMap(param) for param in parameters]:
         # Skip keys that are in versioned repos.
         if not in_versioned_repos.get(key):
-            extra_buildparams[key] = value
+            extra_buildparams[key] = JenkinsParam(type, value)
 
     EXTRA_BUILDPARAMS_CACHE = extra_buildparams
     return extra_buildparams
@@ -1149,7 +1161,7 @@ def trigger_jenkins_build(state, tag_avail):
 
     for param in extra_buildparams.keys():
         if state_value(state, ["extra_buildparams", param]) is None:
-            update_state(state, ["extra_buildparams", param], extra_buildparams[param])
+            update_state(state, ["extra_buildparams", param], extra_buildparams[param].value)
 
     params = None
 
@@ -1225,10 +1237,11 @@ def trigger_jenkins_build(state, tag_avail):
         if param[1] != "":
             postdata.append(("value", param[1]))
 
-        if param[1] == "on":
-            jdata['parameter'].append({"name": param[0], "value": True})
-        elif param[1] == "":
-            jdata['parameter'].append({"name": param[0], "value": False})
+        if extra_buildparams.get(param[0]) is not None and extra_buildparams[param[0]].type == "bool":
+            if param[1] == "on":
+                jdata['parameter'].append({"name": param[0], "value": True})
+            elif param[1] == "":
+                jdata['parameter'].append({"name": param[0], "value": False})
         else:
             jdata['parameter'].append({"name": param[0], "value": param[1]})
 

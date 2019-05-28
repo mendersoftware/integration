c1f51bf2776eaca3e0a7985bf2029cae5333b8ed
Ole Petter <ole.orhagen@northern.tech>
2019-05-28 13:49:44 +0200

    MEN-2433: Automate upload of demo application artifact in demo server
    Changelog: Title
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

---
+++ tests/tests/test_demo_artifact.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# Copyright 2019 Northern.tech AS
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import logging
+import os
+import signal
+import subprocess
+
+import pytest
+import requests
+
+import conftest
+from common_docker import *
+from MenderAPI import *
+from mendertesting import MenderTesting
+
+
+class TestDemoArtifact(MenderTesting):
+    """A simple class for testing the demo-Artifact upload."""
+
+    # NOTE - The password is set on a per test-basis,
+    # as it is generated on the fly by the demo script.
+    auth = authentication.Authentication(
+        username='mender-demo', email='mender-demo@example.com')
+    authv2 = auth_v2_mod.DeviceAuthV2(auth)
+    deploy = deployments.Deployments(auth, authv2)
+
+    @pytest.fixture(scope="function")
+    def run_demo_script(self, request):
+        """Simple fixture which returns a function which runs 'demo up'.
+        Afterwards the fixture brings down the docker-compose environment,
+        so that each invocation run keeps the environment clean."""
+
+        request.addfinalizer(stop_docker_compose)
+
+        def run_demo_script_up():
+            test_env = os.environ.copy()
+            test_env[
+                'DOCKER_COMPOSE_PROJECT_NAME'] = conftest.docker_compose_instance
+            proc = subprocess.Popen(
+                [
+                    './demo', '--client', '-p',
+                    conftest.docker_compose_instance, 'up'
+                ],
+                cwd="..",
+                stdout=subprocess.PIPE,
+                env=test_env)
+            logging.info('Started the demo script')
+            password = ""
+            time.sleep(60)
+            for line in iter(proc.stdout.readline, ''):
+                logging.info(line)
+                if "Login password:" in line.strip():
+                    password = line[-13:-1]
+                    logging.info('The login password:')
+                    logging.info(password)
+                    auth.password = password
+                    assert len(password) == 12
+                    break
+            return proc
+
+        return run_demo_script_up
+
+    # Give the test a timeframe, as the script might run forever,
+    # if something goes awry, or the script is not brought down properly.
+    @pytest.mark.timeout(3000)
+    def test_demo_artifact(self, run_demo_script):
+        """Tests that the demo script does indeed upload the demo Artifact to the server."""
+
+        logging.info("--------------------------------------------------")
+        logging.info("Running test_demo_artifact_upload")
+        logging.info("--------------------------------------------------")
+        self.demo_artifact_upload(run_demo_script)
+        stop_docker_compose()
+        auth.reset_auth_token()
+
+        logging.info("--------------------------------------------------")
+        logging.info("Running test_demo_artifact_installation")
+        logging.info("--------------------------------------------------")
+        self.demo_artifact_installation(run_demo_script)
+        stop_docker_compose()
+        auth.reset_auth_token()
+
+        logging.info("--------------------------------------------------")
+        logging.info("Running test_demo_up_down_up")
+        logging.info("--------------------------------------------------")
+        self.demo_up_down_up(run_demo_script)
+        stop_docker_compose()
+        auth.reset_auth_token()
+
+    def demo_artifact_upload(self, run_demo_script):
+        proc = run_demo_script()
+        arts = deploy.get_artifacts()
+        assert len(arts) == 1
+        assert "mender-demo-artifact" in arts[0]['name']
+        # Emulate ctrl-c exit
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
+        assert proc.returncode == 0
+
+    def demo_artifact_installation(self, run_demo_script):
+        """Tests that the demo-artifact is successfully deployed to a client device."""
+        run_demo_script()
+        artifacts = deploy.get_artifacts()
+        assert len(artifacts) == 1
+        artifact_name = artifacts[0]['name']
+
+        # Trigger the deployment
+        devices = auth_v2.get_devices()
+        assert len(devices) == 1
+
+        # Accept the device to be updated
+        auth_v2.accept_devices(1)
+        devices = list(
+            set([
+                device["id"]
+                for device in auth_v2.get_devices_status("accepted")
+            ]))
+        assert len(devices) == 1
+
+        # Run the deployment.
+        deployment_id = deploy.trigger_deployment(
+            name="Demo artifact deployment",
+            artifact_name=artifacts[0]['name'],
+            devices=devices)
+        deploy.check_expected_status("inprogress", deployment_id)
+
+        # Verify the deployment
+        deploy.check_expected_status("finished", deployment_id)
+
+    def demo_up_down_up(self, run_demo_script):
+        """Test that bringing the demo environment up, then down, then up succeeds"""
+        self.demo_artifact_upload(run_demo_script)
+        self.demo_artifact_upload(run_demo_script)
+        logging.info('Finished')

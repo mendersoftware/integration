5b8c40306501c89779457f395fc269bd36f04a38
Marcin Chalczynski <m.chalczynski@gmail.com>
2019-07-26 08:02:54 +0200

    tests: fix skip for test_00_multi_tenancy
    the condition:
    - is not available anymore
    - was always True
    - didn't make sense at all
    
    the --mt_... arg was never provided. but - it always
    had a default. so, going by this condition alone, the
    test would always run, hence the 'makes no sense'.
    
    what actually controls MT tests is the run_tenant_tests flag,
    but the mechanism is very indirect.
    
    MT fixtures in setup check this flag and do a pytest.skip.
    what this function does is it *skips the entire test* the fixture
    is called for. so this, if anything could be the condition for the test.
    
    but then - this flag is also set to true by default. the only time
    it's set to False is when we're in an integration branch, outside of
    master.
    
    conclusion:
    - don't set any condition
    - let the master vs branch logic set run_tenant_tests
    - keep relying on fixtures to skip out of MT tests according to ^^
    
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

--- tests/tests/test_00_multi_tenancy.py
+++ tests/tests/test_00_multi_tenancy.py
@@ -25,8 +25,6 @@ from common_update import update_image_successful, update_image_failed, \
 from mendertesting import MenderTesting
 from tests import artifact_lock
 
-@pytest.mark.skipif(len(conftest.mt_docker_compose_file) == 0,
-                    reason="set --mt-docker-compose-file to run test")
 class TestMultiTenancy(MenderTesting):
     def mender_log_contains_aborted_string(self, mender_client_container="mender-client"):
         expected_string = "deployment aborted at the backend"

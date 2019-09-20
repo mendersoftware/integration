f955334fac2b3606774ba0a1e58e28571d7a59d3
Marcin Chalczynski <m.chalczynski@gmail.com>
2019-09-20 09:18:21 +0200

    testutils/common: dev creation helpers
    these proved to be reusable as almost all tests copypaste them.
    want to reuse them for OS->ENT migration testing in integration too.
    
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

--- testutils/common.py
+++ testutils/common.py
@@ -188,3 +188,49 @@ def change_authset_status(did, aid, status, utoken):
                                    deviceauth_v2.req_status(status),
                                    path_params={'did': did, 'aid': aid })
     assert r.status_code == 204
+
+def rand_id_data():
+    mac = ":".join(["{:02x}".format(random.randint(0x00, 0xFF), 'x') for i in range(6)])
+    sn = "".join(["{}".format(random.randint(0x00, 0xFF)) for i in range(6)])
+
+    return {'mac': mac, 'sn': sn}
+
+def make_pending_device(utoken, tenant_token=''):
+    id_data = rand_id_data()
+
+    priv, pub = testutils.util.crypto.rsa_get_keypair()
+    new_set = create_authset(id_data, pub, priv, utoken, tenant_token=tenant_token)
+
+    dev = Device(new_set.did, new_set.id_data, pub, tenant_token)
+
+    dev.authsets.append(new_set)
+
+    dev.status = 'pending'
+
+    return dev
+
+def make_accepted_device(utoken, devauthd, tenant_token=''):
+    dev = make_pending_device(utoken, tenant_token=tenant_token)
+    aset_id = dev.authsets[0].id
+    change_authset_status(dev.id, aset_id, 'accepted', utoken)
+
+    aset = dev.authsets[0]
+    aset.status = 'accepted'
+
+    # obtain auth token
+    body, sighdr = deviceauth_v1.auth_req(aset.id_data,
+                                          aset.pubkey,
+                                          aset.privkey,
+                                          tenant_token)
+
+    r = devauthd.call('POST',
+                      deviceauth_v1.URL_AUTH_REQS,
+                      body,
+                      headers=sighdr)
+
+    assert r.status_code == 200
+    dev.token = r.text
+
+    dev.status = 'accepted'
+
+    return dev

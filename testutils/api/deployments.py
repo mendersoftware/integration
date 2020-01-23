30e030b206832eb8bd7b8c10d337fe9760117dd4
Fabio Tranchitella <fabio@tranchitella.eu>
2020-01-23 15:03:53 +0100

    Add test_create_artifact to back-end tests
    Changelog: none
    Signed-off-by: Fabio Tranchitella <fabio@tranchitella.eu>

--- testutils/api/deployments.py
+++ testutils/api/deployments.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Northern.tech AS
+# Copyright 2020 Northern.tech AS
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -22,3 +22,5 @@ URL_STATUS = '/device/deployments/{id}/status'
 URL_DEPLOYMENTS           = '/deployments'
 URL_DEPLOYMENTS_ID        = '/deployments/{id}'
 URL_DEPLOYMENTS_ARTIFACTS = '/artifacts'
+URL_DEPLOYMENTS_ARTIFACTS_GET = '/artifacts/{id}'
+URL_DEPLOYMENTS_ARTIFACTS_GENERATE = '/artifacts/generate'

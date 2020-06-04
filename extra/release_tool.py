78647f31229dbba2a93746583c2c4b10522c5a60
Kristian Amlie <kristian.amlie@northern.tech>
2020-06-04 09:08:19 +0200

    release_tool: Better recovery when tag deletion fails.
    Do the local tag deletion after the remote deletion, instead of vice
    versa. This means that we can immediately pick up where we left off if
    we get an exception for whatever reason (network error, etc.). Without
    this we have to refetch the remote tags in order to delete them
    afterwards.
    
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>
    (cherry picked from commit 495287c1df91fa166af567396cbf49e3e108285b)

--- extra/release_tool.py
+++ extra/release_tool.py
@@ -1818,7 +1818,6 @@ def purge_build_tags(state, tag_avail):
             ):
                 to_purge.append(tag)
         if len(to_purge) > 0:
-            git_list.append((state, repo.git(), ["tag", "-d"] + to_purge))
             git_list.append(
                 (
                     state,
@@ -1826,6 +1825,7 @@ def purge_build_tags(state, tag_avail):
                     ["push", remote] + [":%s" % tag for tag in to_purge],
                 )
             )
+            git_list.append((state, repo.git(), ["tag", "-d"] + to_purge))
 
     query_execute_git_list(git_list)
 

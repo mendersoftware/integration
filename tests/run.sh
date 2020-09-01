0d2c7daf0f356012dc433d8cd2cce9b3158e7393
Lluis Campos <lluis.campos@northern.tech>
2020-09-01 14:03:35 +0200

    [tests/run.sh] Remove obsolete fallback method for client fs images
    Nothing is build for thud nowadays (at least from integration/master)
    and this fallback method will fail with newer images, so better to clean
    up.
    
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/run.sh
+++ tests/run.sh
@@ -121,18 +121,6 @@ function get_requirements() {
     inject_pre_generated_ssh_keys
 }
 
-# Old ways of getting the image, now deprecated, but still needed for images
-# built with thud or older.
-get_ext4_image_deprecated() {
-    if [[ -n "$BUILDDIR" ]]; then
-        cp -f "$BUILDDIR/tmp/deploy/images/$MACHINE_NAME/core-image-full-cmdline-$MACHINE_NAME.ext4" .
-    elif [[ -n "$DOWNLOAD_REQUIREMENTS" ]]; then
-        curl --fail "https://mender.s3-accelerate.amazonaws.com/temp_${MENDER_BRANCH}/core-image-full-cmdline-$MACHINE_NAME.ext4" \
-             -o core-image-full-cmdline-$MACHINE_NAME.ext4 \
-             -z core-image-full-cmdline-$MACHINE_NAME.ext4
-    fi
-}
-
 if [[ $1 == "--get-requirements" ]]; then
     get_requirements
     exit 0
@@ -144,19 +132,13 @@ if [[ -z "$BUILDDIR" ]] && [[ -n "$DOWNLOAD_REQUIREMENTS" ]]; then
     get_requirements
 fi
 
+# Extract file system images from Docker images
 mkdir -p output
-ret=0
 docker run --rm --privileged --entrypoint /extract_fs -v $PWD/output:/output \
        mendersoftware/mender-client-qemu:$(../extra/release_tool.py --version-of mender-client-qemu --version-type docker) || ret=$?
-if [ $ret -eq 0 ]; then
-    # There is `extract_fs` support. Get the R/O image too.
-    docker run --rm --privileged --entrypoint /extract_fs -v $PWD/output:/output \
-           mendersoftware/mender-client-qemu-rofs:$(../extra/release_tool.py --version-of mender-client-qemu-rofs --version-type docker)
-    mv output/* .
-else
-    # Old style ext4 fetching.
-    get_ext4_image_deprecated
-fi
+docker run --rm --privileged --entrypoint /extract_fs -v $PWD/output:/output \
+        mendersoftware/mender-client-qemu-rofs:$(../extra/release_tool.py --version-of mender-client-qemu-rofs --version-type docker)
+mv output/* .
 rmdir output
 
 modify_services_for_testing

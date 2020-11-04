b9f59d242ba695b8fb0205984097c000df1afda2
Marcin Chalczynski <m.chalczynski@gmail.com>
2020-11-05 00:14:38 +0100

    FIX test_devauth, testutils: after service restart, wait for traefik
    dynamic configuration means we have to wait a bit for it to
    take effect.
    
    in this case, after killing the service, traefik uninstalls
    the associated router and service. even when the service is back up,
    traefik might not have yet noticed, and fail/reroute the requests, so:
    
    - wait a bit for the restarted router/service to be registered again.
    
    very sneaky errors can be experienced otherwise:
     - in this case, POST /auth_requests would go to mender-gui(!),
    because of its catchall regex middlewares.
    - resulted in: 405(!) from node's nginx (POST on a static resource...)
    
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

--- testutils/common.py
+++ testutils/common.py
@@ -19,6 +19,8 @@ import string
 import tempfile
 import os
 import subprocess
+import redo
+import requests
 from contextlib import contextmanager
 
 import testutils.api.deviceauth as deviceauth
@@ -315,3 +317,42 @@ def get_mender_artifact(
     finally:
         os.unlink(filename)
         os.path.exists(artifact) and os.unlink(artifact)
+
+
+def wait_for_traefik(gateway_host, routers=[]):
+    """ Wait until provided routers are installed.
+    Prevents race conditions where services are already up but traefik hasn't yet registered their routers. This causes subtle timing issues.
+    By default checks the basic routers (incl. deployments - startup so time consuming, in practice it guarantees success).
+    TODO: k8s support?
+    """
+    if routers == []:
+        rnames = [
+            "deployments@docker",
+            "deploymentsMgmt@docker",
+            "minio@docker",
+            "deviceauth@docker",
+            "deviceauthMgmt@docker",
+            "newInventory@docker",
+            "oldInventory@docker",
+            "useradm@docker",
+            "useradmLogin@docker",
+            "deviceauth@docker",
+            "deviceauthMgmt@docker",
+            "oldDeviceInventory@docker",
+        ]
+    else:
+        rnames = routers[:]
+
+    for _ in redo.retrier(attempts=5, sleeptime=10):
+        try:
+            r = requests.get("http://{}:8080/api/http/routers".format(gateway_host))
+            assert r.status_code == 200
+
+            cur_routers = [x["name"] for x in r.json()]
+
+            if set(cur_routers).issuperset(set(rnames)):
+                break
+        except requests.exceptions.ConnectionError as ex:
+            print("connection error while waiting for routers - but that's ok")
+    else:
+        assert False, "timeout hit waiting for traefik routers {}".format(rnames)

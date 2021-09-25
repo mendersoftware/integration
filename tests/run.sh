064be958d4a9d6f289ec6ddcba3a0bf25d8c08f7
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-09-25 07:17:27 +0200

    Do not change the global kubeconfig file, use a custom one
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- tests/run.sh
+++ tests/run.sh
@@ -181,7 +181,8 @@ fi
 export TENANTADM_STRIPE_API_KEY=$STRIPE_API_KEY
 
 if [ -n "$K8S" ]; then
-    aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $AWS_EKS_CLUSTER_NAME
+    export KUBECONFIG="${HOME}/kubeconfig.${K8S}"
+    aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $AWS_EKS_CLUSTER_NAME --kubeconfig ${HOME}/kubeconfig.${K8S}
     kubectl config set-context --current --namespace=$K8S
     kubectl get pods -o wide
 fi

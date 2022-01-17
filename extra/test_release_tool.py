mendersoftware/integration

commit 2ae692cc60d0f2a02d956ce5f7277bc6d28d1211

release_tool: Add the option to version the components-map file
Changelog: None
Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

extra/release_tool.py
@@ -77,9 +77,14 @@ class Component:
     name = None
     type = None
 
+    _integration_version = None
+
     def __init__(self, name, type):
         self.name = name
         self.type = type
+
+    def set_integration_version(version):
+        Component._integration_version = version
 
     def git(self):
         if self.type != "git":
@@ -112,8 +117,18 @@ class Component:
     @staticmethod
     def _initialize_component_maps():
         if Component.COMPONENT_MAPS is None:
-            with open(os.path.join(integration_dir(), "component-maps.yml")) as fd:
-                Component.COMPONENT_MAPS = yaml.safe_load(fd)
+            if Component._integration_version:
+                component_maps = execute_git(
+                    None,
+                    integration_dir(),
+                    ["show", f"{Component._integration_version}:component-maps.yml"],
+                    capture=True,
+                    capture_stderr=False,
+                )
+                Component.COMPONENT_MAPS = yaml.safe_load(component_maps)
+            else:
+                with open(os.path.join(integration_dir(), "component-maps.yml")) as fd:
+                    Component.COMPONENT_MAPS = yaml.safe_load(fd)
 
     @staticmethod
     def get_component_of_type(type, name):
@@ -644,6 +659,7 @@ def do_version_of(args):
     """Process --version-of argument."""
 
     try:
+        Component.set_integration_version(args.in_integration_version)
         comp = Component.get_component_of_any_type(args.version_of)
     except KeyError:
         print("Unrecognized repository: %s" % args.version_of)

commit f56785d6c204e53daa3f869ea7e949b2c14f3694

release_tool: Fix ranges with `-i` which was broken in 2ae692cc60d.
Changelog: None

Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

extra/release_tool.py
@@ -74,7 +74,14 @@ class Component:
         self.type = type
 
     def set_integration_version(version):
-        Component._integration_version = version
+        # If version is a range, use the later version.
+        if version is not None:
+            version = version.split("..")[-1]
+
+        if Component._integration_version != version:
+            Component._integration_version = version
+            # Invalidate cache.
+            Component.COMPONENT_MAPS = None
 
     def git(self):
         if self.type != "git":

extra/test_release_tool.py
@@ -259,6 +259,13 @@ def test_version_of_with_in_integration_version(capsys):
         "1.7.0",
     )
 
+    # Ranges
+    run_main_assert_result(
+        capsys,
+        ["--version-of", "inventory", "--in-integration-version", "3.0.1..3.1.0",],
+        "3.0.0..4.0.0",
+    )
+
 
 def test_set_version_of(capsys, is_staging):
     # Using --set-version-of modifies both versions, regardless of using the repo name
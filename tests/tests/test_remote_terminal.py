b490637c7b1f738f2916f5c5236e72f189553fdb
Kristian Amlie <kristian.amlie@northern.tech>
2022-06-15 10:50:18 +0200

    test: Try to mitigate common spurious errors with flaky plugin.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- tests/tests/test_remote_terminal.py
+++ tests/tests/test_remote_terminal.py
@@ -13,6 +13,7 @@
 #    limitations under the License.
 #
 
+from flaky import flaky
 import subprocess
 import time
 
@@ -23,6 +24,7 @@ from .common_connect import wait_for_connect
 from .mendertesting import MenderTesting
 
 
+@flaky(max_runs=3)
 class BaseTestRemoteTerminal(MenderTesting):
     """Tests the remote terminal functionality"""
 

bd9f2244567c1e9f4793b3413c7a9986dec971cf
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2022-07-20 16:05:14 +0200

    test: improve mtls tests reducing the number of docker compose commands
    Changelog: None
    Ticket: None
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>
    (cherry picked from commit 6b668616fa373914412e231dddbbfe5c5955ee69)

--- testutils/infra/container_manager/docker_compose_manager.py
+++ testutils/infra/container_manager/docker_compose_manager.py
@@ -476,19 +476,13 @@ class DockerComposeMTLSSetup(DockerComposeNamespace):
         self._wait_for_containers()
 
     def start_api_gateway(self):
-        self._docker_compose_cmd(
-            "up -d --scale mender-api-gateway=1 mender-api-gateway"
-        )
+        self._docker_compose_cmd("start mender-api-gateway")
 
     def stop_api_gateway(self):
-        self._docker_compose_cmd(
-            "up -d --scale mender-api-gateway=0 mender-api-gateway"
-        )
+        self._docker_compose_cmd("stop mender-api-gateway")
 
     def start_mtls_ambassador(self):
-        self._docker_compose_cmd(
-            "up -d --scale mtls-ambassador=1 --scale mender-client=0 mtls-ambassador mender-client"
-        )
+        self._docker_compose_cmd("up -d --scale mtls-ambassador=1 mtls-ambassador")
         self._wait_for_containers()
 
     def new_mtls_client(self, name, tenant):

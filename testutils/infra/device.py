2b7f47203051ee222a613a23b0dfea0b2d68c612
Ole Petter <ole.orhagen@northern.tech>
2023-09-20 13:53:54 +0200

    chore(cpp-client): Return the correct systemd service name
    At least the correct one for mender-update.
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- testutils/infra/device.py
+++ testutils/infra/device.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Northern.tech AS
+# Copyright 2023 Northern.tech AS
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -153,7 +153,7 @@ class MenderDevice:
     def get_client_service_name(self):
         if self._service_name is None:
             self._service_name = self.run(
-                "if test -e /lib/systemd/system/mender.service; then echo mender; else echo mender-client; fi"
+                "echo mender-updated"
             ).strip()
         return self._service_name
 

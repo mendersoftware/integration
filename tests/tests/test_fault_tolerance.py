3c9878cbf35802addbcf830894e32d5d9a7612a1
Ole Petter <ole.orhagen@northern.tech>
2023-10-30 14:06:18 +0100

    test(fault-tolerance): Delete the block-after-start test
    The test is now outdated, since we have moved our fetch and store logic.
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- tests/tests/test_fault_tolerance.py
+++ tests/tests/test_fault_tolerance.py
@@ -32,10 +32,6 @@ from .mendertesting import MenderTesting
 from testutils.infra.container_manager.kubernetes_manager import isK8S
 
 DOWNLOAD_RETRY_TIMEOUT_TEST_SETS = [
-   {
-        "blockAfterStart": False,
-        "logMessageToLookFor": "[uU]pdate (check|fetch) failed:",
-    },
     {"blockAfterStart": True, "logMessageToLookFor": "Download connection broken:"},
 ]
 

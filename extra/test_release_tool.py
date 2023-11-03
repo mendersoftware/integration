145ff2f27b9cb25e7c490b8be53d4d5de47f24e4
Lluis Campos <lluis.campos@northern.tech>
2023-11-03 14:33:32 +0100

    test: Make `test_release_tool.py` pass in feature branch
    So that we can merge green and shinny PRs.
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- extra/test_release_tool.py
+++ extra/test_release_tool.py
@@ -102,13 +102,15 @@ def test_version_of(capsys, is_master):
 
     # Querying mender with version-type docker should error, since it maps to
     # multiple containers and we don't know which one to choose.
-    run_main_assert_result(capsys, ["--version-of", "mender"], "master")
+    run_main_assert_result(capsys, ["--version-of", "mender"], "feature-c++-client")
     with pytest.raises(Exception):
         run_main_assert_result(
             capsys, ["--version-of", "mender", "--version-type", "docker"], "master"
         )
     run_main_assert_result(
-        capsys, ["--version-of", "mender", "--version-type", "git"], "master"
+        capsys,
+        ["--version-of", "mender", "--version-type", "git"],
+        "feature-c++-client",
     )
 
     # For an independent component, it should also error because it doesn't have

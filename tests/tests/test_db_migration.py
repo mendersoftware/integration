9f3921941159741018f8c9c58e94f06bc162dd83
Ole Petter <ole.orhagen@northern.tech>
2023-11-09 13:34:21 +0100

    chore: re-enable the DB migration tests
    Ticket: MEN-6837
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- tests/tests/test_db_migration.py
+++ tests/tests/test_db_migration.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Northern.tech AS
+# Copyright 2023 Northern.tech AS
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -27,9 +27,6 @@ from ..MenderAPI import DeviceAuthV2, Deployments, logger
 from .mendertesting import MenderTesting
 
 
-@pytest.mark.skipif(
-    not (os.environ.get("NIGHTLY_BUILD", "false") == "true"), reason="MEN-6671",
-)
 class BaseTestDBMigration(MenderTesting):
     def ensure_persistent_conf_script(self, dir):
         # Because older versions of Yocto branches did not split mender.conf

e3b6ccb50a065a2e909d4a868a11cf9ddf7cc7d8
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2025-06-12 09:40:00 +0200

    test: mark test_tcp_teardown with xfail
    This test consistently fails when the full integration tests are
    triggered from mender-qa
    
    Ticket: QA-1076
    Ticket: QA-1056
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
    (cherry picked from commit d8c33fe8fb65aa8bdef8732f361900f4c3b7ca85)

--- tests/tests/test_tcp_teardown.py
+++ tests/tests/test_tcp_teardown.py
@@ -14,6 +14,7 @@
 #
 
 import json
+import pytest
 import os.path
 import tempfile
 import time
@@ -102,9 +103,11 @@ class BaseTestTcpTeardown:
         assert get_opened_tcp_connections(mender_device, "mender-auth") == 0
 
 
+@pytest.mark.xfail(reason="QA-1076 and QA-1056")
 class TestTcpTeardownOpenSource(BaseTestTcpTeardown):
     pass
 
 
+@pytest.mark.xfail(reason="QA-1076 and QA-1056")
 class TestTcpTeardownEnterprise(BaseTestTcpTeardown):
     pass
